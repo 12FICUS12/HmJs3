0ffb220f70358595300ceeee5ca405bffeaa6cd8
Maria <maria.Alekseeva14@mail.ru>
2024-11-06 13:41:25 +0300

    revision

--- task2/task.js
+++ task2/task.js
@@ -1,18 +1,19 @@
 const rotatorCases = document.querySelectorAll('.rotator__case');
 let currentIndex = 0;
+
 function rotate() {
-    rotatorCases[currentIndex].classList.remove('rotatorcase_active');
+    rotatorCases[currentIndex].classList.remove('rotator__case_active');
     currentIndex = (currentIndex + 1) % rotatorCases.length;
 
     const nextCase = rotatorCases[currentIndex];
     const speed = nextCase.getAttribute('data-speed');
     const color = nextCase.getAttribute('data-color');
-    nextCase.style.color = color;
 
-    nextCase.classList.add('rotatorcase_active');
+    nextCase.style.color = color;
+    nextCase.classList.add('rotator__case_active');
     setTimeout(rotate, speed);
 }
 
-// Запускаем функцию rotate с начальным значением скорости
+
 const initialSpeed = rotatorCases[currentIndex].getAttribute('data-speed');
 setTimeout(rotate, initialSpeed);
